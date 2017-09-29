4d4d0c7df2588ffabf4cd0ece08db65dfd92cf3d
longdsh <212977541@qq.com>
2017-09-29 17:28:51 +0800

    17/9/29

--- src/main/java/com/acm/service/impl/UserDeptServiceImpl.java
+++ src/main/java/com/acm/service/impl/UserDeptServiceImpl.java
@@ -1,20 +1,28 @@
 package com.acm.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.acm.dao.UserDeptMapper;
 import com.acm.entity.UserDept;
 import com.acm.service.UserDeptService;
 
+@Service
 public class UserDeptServiceImpl implements UserDeptService {
 
+	@Autowired
+	UserDeptMapper userDeptMapper;
 	@Override
 	public void userAddDept(UserDept userDept) {
 		// TODO Auto-generated method stub
+		userDeptMapper.insert(userDept);
 
 	}
 
 	@Override
 	public void userDelectDept(Integer deptId) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
