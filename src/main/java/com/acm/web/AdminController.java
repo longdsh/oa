57eb91059baf620ed4319925373001ab60c52f9b
longdsh <2512977541@qq.com>
2017-10-03 13:55:34 +0800

    1

--- src/main/java/com/acm/web/AdminController.java
+++ src/main/java/com/acm/web/AdminController.java
@@ -1,8 +1,17 @@
 package com.acm.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.acm.entity.Department;
+import com.acm.entity.User;
+import com.acm.service.impl.DepartmentServiceImpl;
+import com.acm.service.impl.UserServiceImpl;
 
 /**
 * @author 计算机网络软件应用1501 路素飞
@@ -14,8 +23,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/admin")
 public class AdminController {
 	
+	@Autowired
+	UserServiceImpl userServiceImpl;
+	@Autowired
+	DepartmentServiceImpl departmentServiceImpl;
+	
 	@RequestMapping(value="/adminFrist",method = {RequestMethod.POST, RequestMethod.GET})
-	public String adminFrist() {
+	public String adminFrist(Model model) {
+		List<Department> departments = departmentServiceImpl.findAllDepartment();
+		model.addAttribute("departments", departments);
 		return "admin";
 	}
 }
--- src/main/webapp/WEB-INF/pages/admin.jsp
+++ src/main/webapp/WEB-INF/pages/admin.jsp
@@ -1,15 +1,25 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>管理员页面</title>
+<%
+	pageContext.setAttribute("APP_PATH", request.getContextPath());
+%>
 </head>
 <body>
    admin <a href="${pageContext.request.contextPath}/logout">注销</a>
    <a href="${pageContext.request.contextPath}/admin/adminFrist">admin</a>
    <a href="${pageContext.request.contextPath}/dept/deptFrist">dept</a>
    <a href="${pageContext.request.contextPath}/user/userFrist">user</a>
+   
+   <c:forEach var="dept" items="${departments }">
+      ${dept.id }
+      ${dept.name}
+   </c:forEach>
 </body>
 </html>
